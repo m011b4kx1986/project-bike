4c42a16daf7be80c0c0708c50b5eaf9f5112da0c
m011b4kx1986 <m011b4kx1986@bangkit.academy>
2024-10-04 12:00:52 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import pyplot as plt
 import numpy as np  
 import seaborn as sns
 
